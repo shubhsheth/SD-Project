bb7fecc0d582c9caa6a7fc23b26d175b72f8b37c
JLopezz3 <lopezjerel@gmail.com>
2021-02-09 16:31:50 -0600

    setup DB

--- server/db/db.js
+++ server/db/db.js
@@ -1,9 +1,9 @@
 const mysql = require("mysql");
 const db = mysql.createConnection({
-  host: "us-cdbr-east-03.cleardb.com",
-  user: "b0d6684dad18f0",
-  password: "6dede834",
-  database: "heroku_49b1bd5c5457661",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = db;
--- server/index.js
+++ server/index.js
@@ -1,19 +1,30 @@
 const express = require("express");
 const app = express();
-const db = require("./db/db");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const apiRouter = require("./routes");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const db = require("./db/db");
+
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-//Home page
+// Home page
 app.get("/", (req, res) => {
-  res.send("hello world");
+  // res.send(process.env.DB_PASSWORD);
+  let a = db.query(`SELECT * FROM ${process.env.DB_NAME}.persons`);
+  res.send(a);
 });
 
-//server listening on PORT 5000, WILL CHANGE PORT to a CONST later.
-app.listen(5000, () => {
-  console.log("Server running on PORT 5000");
+app.use("/api/chirps", apiRouter);
+
+//server listening on PORT 5000
+app.listen(process.env.PORT || "5000", () => {
+  console.log(`Server is running on port: ${process.env.PORT || "5000"}`);
 });
+
