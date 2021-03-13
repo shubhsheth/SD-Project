2dc1afa046339cdf427d61e0345ba1743c60ac94
Shubh Sheth <shubhsheth8@gmail.com>
2021-03-13 17:39:52 -0600

    Formatting and Updated Error Reporting

--- server/index.js
+++ server/index.js
@@ -6,9 +6,7 @@ const app = express();
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
-const apiRouter = require("./routes");
 
-const db = require("./db/db");
 const quote = require("./routes/quote");
 const login = require("./routes/login");
 
@@ -16,16 +14,12 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((err, req, res, next) => {
-  // This check makes sure this is a JSON parsing issue, but it might be
-  // coming from any middleware, not just body-parser:
-
   if (err instanceof SyntaxError && err.status === 400 && 'body' in err) {
       console.error(err);
       return res.sendStatus(400); // Bad request
   }
   next();
 });
-app.use("/api/chirps", apiRouter);
 
 //server listening on PORT 5000
 app.listen(process.env.PORT || "5000", () => {
@@ -33,9 +27,9 @@ app.listen(process.env.PORT || "5000", () => {
 });
 
 // Login
-app.get('/login', login.authUser);
-app.get('/register', login.addUser);
-app.get('/update-profile', login.addUserProfile);
+app.post('/login', login.authUser);
+app.post('/register', login.addUser);
+app.post('/update-profile', login.addUserProfile);
 
 
 // Quotes
