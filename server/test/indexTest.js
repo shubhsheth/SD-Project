976e83154bf24ff386420830eddb362075bf8244
Shubh Sheth <shubhsheth8@gmail.com>
2021-03-16 00:33:19 -0500

    Added testing for all other endpoints

--- server/test/indexTest.js
+++ server/test/indexTest.js
@@ -62,12 +62,70 @@ describe('Task API', () => {
     });
 
     /**
+     * Update Profile
+     */
+    describe("POST /profile-management", () => {
+        it("It should update profile", (done) => {
+            const user = {
+                "userId": 14,
+                "fullname": "z",
+                "address1": "test1",
+                "address2": "test2",
+                "city": "test3",
+                "state": "test4",
+                "zip": "test5"
+            }
+            chai.request(server)
+                .post("/profile-management")
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+
+    /**
      * Get Quote
      */
+     describe("GET /quote", () => {
+        it("It should get quote", (done) => {
+            const quote = {
+                "location": "Texas",
+                "gallons": 1000,
+                "userid": 14
+            }
+            chai.request(server)
+                .get("/quote")
+                .send(quote)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.margin.should.be.eq(0.225);
+                    res.body.quote.should.be.eq(1.725);
+                    res.body.total.should.be.eq(1725);
+                    done();
+                });
+        });
+    });
 
     /**
      * Get Quote History
      */
+     describe("GET /fuel-history", () => {
+        it("It should get history", (done) => {
+            const user = {
+                "userid": 14
+            }
+            chai.request(server)
+                .get("/fuel-history")
+                .send(user)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.length.should.be.above(0);
+                    done();
+                });
+        });
+    });
 
     return;
 });
