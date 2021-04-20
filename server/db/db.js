fc27702a2d3072579601ce8a66d5da3764850a47
Shubh Sheth <shubhsheth8@gmail.com>
2021-04-19 22:22:03 -0500

    DB connection updatse

--- server/db/db.js
+++ server/db/db.js
@@ -16,7 +16,9 @@ function handleDisconnect() {
 		if (err) {                                     	// or restarting (takes a while sometimes).
 			console.log('error when connecting to db:', err);
 			setTimeout(handleDisconnect, 2000); 		// We introduce a delay before attempting to reconnect,
-		}                                     			// to avoid a hot loop, and to allow our node script to
+		} else {
+			console.log('connected');
+		}                                    			// to avoid a hot loop, and to allow our node script to
 	});                                     			// process asynchronous requests in the meantime.
 														// If you're also serving http, display a 503 error.
 	connection.on('error', function (err) {
